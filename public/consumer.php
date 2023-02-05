afbffe0bf522d3bbfa9a6100c8512fd7d7b992e0
Ivan Sardyko <sardyco@gmail.com>
2023-02-05 15:02:38 +0400

    - fix version php and kafka - improve consumer

--- public/consumer.php
+++ public/consumer.php
@@ -8,10 +8,10 @@ $topic = $consumer->newTopic("test");
 
 $topic->consumeStart(0, RD_KAFKA_OFFSET_BEGINNING);
 
-echo "consumer started";
+echo "consumer started" . PHP_EOL;
 while (true) {
     $msg = $topic->consume(0, 1000);
-    if ($msg->payload) {
-        echo $msg->payload, "\n";
+    if (isset($msg->payload)) {
+        echo $msg->payload . PHP_EOL;
     }
 }
